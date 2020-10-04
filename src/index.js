faddd942afa98e5a3e2293396bd602529944bbb7
Kseniya <55946556+kseniya1706@users.noreply.github.com>
2020-10-04 13:53:23 +0300

    feat: task compleated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if ((typeof array == "undefined")||(array.length == 0)) {
+    return 0;
+  }
+  array.sort(function(a,b){ 
+    return a - b
+  });
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((typeof array == "undefined")||(array.length == 0)) {
+    return 0;
+  }
+  array.sort(function(a,b){ 
+    return b - a
+  });
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((typeof array == "undefined")||(array.length == 0)) {
+    return 0;
+  }
+  let sum = 0;
+ array.forEach(item => {
+    sum = sum + item;
+  })
+  return sum/(array.length);
 }
